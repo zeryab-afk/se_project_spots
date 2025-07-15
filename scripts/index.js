1bbd89238c5436c53083122a71db3632b90cf893
zeryab-afk <zaryabshafiq54@gmail.com>
2025-07-16 03:15:52 +0500

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+// === INITIAL CARDS DATA ===
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+  }
+];
+
 // === SELECT ELEMENTS ===
 const editProfileButton = document.querySelector('.profile__edit-btn');
 const newPostButton = document.querySelector('.profile__add-btn');
@@ -89,4 +117,9 @@ document.addEventListener('click', (e) => {
       closeModal(modal);
     }
   });
+});
+
+// === LOG CARD NAMES ===
+initialCards.forEach((card) => {
+  console.log(card.name);
 });
